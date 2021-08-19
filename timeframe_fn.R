d0a27475cc972e93d43e1981d0a70fc59deec9b1
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-08-19 15:22:52 -0400

    Add helper fn make_clocks_in_seconds() and add template for add_time_taken()

--- timeframe_fn.R
+++ timeframe_fn.R
@@ -9,6 +9,7 @@ Functions for article:
 "
 
 #### imports ####
+library(dplyr)
 
 
 #### helper fn ####
@@ -27,4 +28,78 @@ load_data <- function(k_games=c(200,500), use_local_file=TRUE)
   df <- read.csv(fpath, stringsAsFactors = T)
   return(df)
 }
+
+
+#### clocks ####
+
+# create var time_taken
+# class(df$Clock_ply_1)
+# x <- as.character(df$Clock_ply_1)
+# x <- strsplit(x, split=":", fixed = T)
+# x <- lapply(x, as.numeric) 
+# x <- sapply(x, \(t) t[1]*60*60 + t[2]*60 + t[3])
+
+
+# helper fn: make the Clock_ply_ vars into seconds
+make_clocks_in_seconds <- function(df)
+{
+  "
+  input: df
+  output: df with Clock_ply_ vars reformatted into seconds
+  "
+  
+  # select vars
+  v <- (c <- colnames(df))[substr(c, 1, 10)=="Clock_ply_"]
+  
+  # as char
+  df[,v] <- sapply(df[,v], as.character)
+  
+  # as seconds
+  times <- sapply(df[,v], strsplit, split=":", fixed = T)
+  times <- apply(times, 2, lapply, as.numeric)
+  times <- lapply(times, sapply, \(t) t[1]*60*60 + t[2]*60 + t[3]) # list of length length(v)
+  times <- as.data.frame(Map(cbind, times)) # as matrix, then as df
+  
+  # replace columns
+  df[,v] <- times
+  
+  # out
+  return(df)
+}
+
+
+# helper fn: add 
+
+
+# add time_taken variables
+add_time_taken <- function(df)
+{
+  "
+  
+  "
+  # make clocks in seconds
+  df <- make_clocks_in_seconds(df)
+  
+  # subtract increment
+  
+  
+  # out
+  print("")
+  return(df)
+}
+
+
+#### evals ####
+
+# create var eval_change
+
+
+
+
+
+
+
+
+
+
 
