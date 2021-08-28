376335590804496369948a70ec58d3e51b02c41b
Patrick Coulombe <patrick.coulombe@d22consulting.com>
2021-08-28 18:20:30 +0000

    Add autocorrelation with time taken series

--- time_series.R
+++ time_series.R
@@ -21,7 +21,7 @@ plot(dw_dl, db_dl)
 
 
 # cor
-cor(dw, db) # <0
+cor(dw, db) # <0 --> penser aux valeurs comparées (plies), est-ce que ce sont les bonnes? white vs black (pense que oui)
 dl <- list(dw_dl, db_dl)
 # replace ±Inf with extreme value
 dl <- lapply(dl, \(t) { t[is.infinite(t)] <- NA; t})
@@ -29,3 +29,29 @@ cor(dl[[1]], dl[[2]], use="pairwise") # <0
 
 # distribution of correlations/acf across games
 # posterior distro bayesian style
+# outliers could be interesting as well
+
+
+# acf
+# acf lag 1
+cor(dw[-length(dw)], dw[-1]) # .11
+cor(db[-length(db)], db[-1]) # .25
+
+acf(dw, lag.max = 1, plot = F) # .11
+acf(dw, lag.max = 2, plot = F) # .157
+
+acf(db, lag.max = 1, plot = F) # .25
+acf(db, lag.max = 2, plot = F) # .296
+
+# plots for all lags
+acf(dw, plot=T)
+acf(db, plot=T)
+
+
+# find sequences of moves by finding large autocorrelations and then drop?
+# though looking at plots, doesn't seem too interesting with time taken
+
+
+
+
+
