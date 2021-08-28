49244649306bb2177ad90e3a730386bb943e549c
Patrick Coulombe <patrick.coulombe@d22consulting.com>
2021-08-28 16:01:09 +0000

    Add time_series.R

---
+++ time_series.R
@@ -0,0 +1,10 @@
+# use time taken vars
+v <- paste0("Time_taken_ply_", 3:200)
+
+# do white and black for a single game
+dw <- (dw<-df[8,v[seq(1, length(v), by=2)]])[!is.na(dw)] # white
+db <- (db<-df[8,v[seq(2, length(v), by=2)]])[!is.na(db)] # black
+
+# make equal length
+l <- min(length(dw), length(db))
+dw <- dw[1:l]; db <- db[1:l]
